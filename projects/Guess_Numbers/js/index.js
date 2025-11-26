4bb3f50867f89c601a6c42644d9813e7089e69fd
Steve <falecomstevevaz@gmail.com>
2025-11-25 23:21:10 -0300

    Creating side bar

---
+++ projects/Guess_Numbers/js/index.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("imySideBar").style.width = "350px";
+    document.body.style.marginLeft = "350px";
+    document.body.style.backgroundImage = "url(./img/keyhole-final-2.png)";
+}
+
+function closeNav() {
+    document.getElementById("icloseBtn").style.width = "0";
+    document.body.style.marginLeft = "0";
+    document.body.style.backgroundColor =  "#223240";
+}
