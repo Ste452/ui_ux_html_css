d894c72f380477c9735ed122d60f5fba688ef8ef
Steve <falecomstevevaz@gmail.com>
2025-11-26 21:58:44 -0300

    focus on game logic in JavaScript

--- projects/Guess_Numbers/js/index.js
+++ projects/Guess_Numbers/js/index.js
@@ -1,11 +1,31 @@
 function openNav() {
     document.getElementById("imySideBar").style.width = "350px";
     document.body.style.marginLeft = "350px";
-    document.body.style.backgroundImage = "url(./img/keyhole-final-2.png)";
+    document.body.style.backgroundImage = "";
 }
 
 function closeNav() {
-    document.getElementById("icloseBtn").style.width = "0";
+    document.getElementById("imySideBar").style.width = "0";
     document.body.style.marginLeft = "0";
     document.body.style.backgroundColor =  "#223240";
+}
+
+function guessNumber() {
+    var possibilities = document.querySelector("button").values;
+    var correctNumber = Math.random(100);
+    
+    do {
+
+
+
+    } while (possibilities != correctNumber);
+}
+
+function refactorGame() {
+    document.querySelector("button").values;
+}
+
+function callAllFunctions() {
+    guessNumber();
+    refactorNumber();
 }
